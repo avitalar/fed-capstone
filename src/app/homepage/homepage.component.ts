98359115ea2338ad5565eb1036ff608f7e17f3c5
avitalar <avitalar1@gmail.com>
2019-06-07 23:39:38 +0300

    featured

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -10,15 +10,35 @@ import { ProdApi } from '../prod-api';
 })
 export class HomepageComponent implements OnInit {
   searchResults: ProdApi;
+  favorites: Array<object> = [];
+  favoriteItems: Array<object> = [];
+  featuredItem: Array<object> = [];
+  element: Array<object> = [];
 
 // tslint:disable-next-line: no-shadowed-variable
   constructor(private ProductsDataService: ProductsDataService) { }
 
   ngOnInit() {
-    this.ProductsDataService.prodData('all').then( (response) => {
+    this.ProductsDataService.prodData().then( (response) => {
       this.searchResults = response;
+      // populate favorites array
+
+
+
+// tslint:disable-next-line: max-line-length
+      this.featuredItem = [response[1].subcategories[0].items[0], response[1].subcategories[1].items[0], response[0].subcategories[2].items[1], response[1].subcategories[2].items[3], response[0].subcategories[1].items[1], response[0].subcategories[2].items[2], response[0].subcategories[1].items[1], response[0].subcategories[1].items[2], response[2].subcategories[1].items[2], response[2].subcategories[1].items[2], response[3].subcategories[1].items[1], response[0].subcategories[1].items[2]];
+      let i = 0;
+      this.featuredItem.forEach(element => {
+        i++;
+        if (element != null) {
+          if (this.favoriteItems.indexOf(element) < 0) { // check if already exist in array to prevent duplicates
+            this.favoriteItems.push(element);
+          }
+      } else {
+      }
+      });
     }, (error) => {
-      alert('Error: ' + error.statusText);
+      console.log('Error: ' + error.statusText);
     });
   }
 
--- src/app/products-data.service.ts
+++ src/app/products-data.service.ts
@@ -6,17 +6,11 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ProductsDataService {
-  cachedValues: Array<{
-    [query: string]: ProdApi
-  }> = [];
   constructor(private http: HttpClient) {
   }
 
-  prodData = (query: string): Promise<ProdApi> => {
+  prodData = (): Promise<ProdApi> => {
     const promise = new Promise<ProdApi>((resolve, reject) => {
-        if (this.cachedValues[query]) {
-            resolve(this.cachedValues[query]);
-        } else {
             this.http.get('https://webmppcapstone.blob.core.windows.net/data/itemsdata.json')
             .toPromise()
             .then( (response) => {
@@ -24,7 +18,6 @@ export class ProductsDataService {
             }, (error) => {
                 reject(error);
             });
-        }
     });
     return promise;
   }
