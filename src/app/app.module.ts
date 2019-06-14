eb34294910f80d7dfc84890b1fc282a7043ca35a
avitalar <avitalar1@gmail.com>
2019-06-14 19:15:08 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,45 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { OwlModule } from 'ngx-owl-carousel';
 import {NgxImageCompressService} from 'ngx-image-compress';
 import { CartComponent } from './cart/cart.component';
+import { ShopComponent } from './shop/shop.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
-
+const appRoutes: Routes = [
+  { path: '',
+    component: HomepageComponent
+  },
+  { path: 'shop',
+    component: ShopComponent,
+    data: {
+      title: 'Shop page'
+    }
+  },
+  { path: 'cart',
+    component: CartComponent,
+    data: {
+      title: 'cart page'
+    }
+  },
+  { path: '**', component: NotFoundComponentComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    CartComponent
+    CartComponent,
+    ShopComponent,
+    NotFoundComponentComponent
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    OwlModule
+    OwlModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [ProductsDataService,NgxImageCompressService],
   bootstrap: [AppComponent]
