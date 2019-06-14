d760d284d6560902fe148dbb2e476f2271b87fd2
avitalar <avitalar1@gmail.com>
2019-06-14 19:47:43 +0300

    shop init

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsDataService} from '../products-data.service';
+import { ProdApi } from '../prod-api';
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  searchResults: ProdApi;
+  allProducts: Array<object> = [];
 
-  constructor() { }
+
+  constructor(private ProductsDataService: ProductsDataService) {   }
 
   ngOnInit() {
-  }
+    this.ProductsDataService.prodData().then( (response) => {
+      this.searchResults = response;
+      // populate favorites array
+      const responseArray = [];
+      responseArray.push(this.searchResults);
+      responseArray[0].forEach(category => {
+         category.subcategories.forEach(subcategory => {
+          subcategory.items.forEach(item => {
+           this.allProducts.push(item);
+          });
+       });
+      });
+}, (error) => {
+  console.log('Error: ' + error.statusText);
+});
+}
 
 }
