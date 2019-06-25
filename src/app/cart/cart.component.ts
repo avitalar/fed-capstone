2c3c9357e2c6cdf6d9452d54c0c8d93a77310c5d
avitalar <avitalar1@gmail.com>
2019-06-25 23:26:52 +0300

    cart total done

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
   cartArray: Array<object>;
   quantValue: number;
+  cartSubtotal: number;
+  ShippingValue = 10;
   quantityFunction(value: number) {
     this.quantValue = value;
   }
@@ -15,14 +17,13 @@ export class CartComponent implements OnInit {
     const cartArray = JSON.parse(localStorage.getItem('cart')) || [];
 // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < cartArray.length; i++) {
-      console.log(typeof(cartArray[i].quantValue));
-      console.log(typeof(this.quantValue));
       if (cartArray[i].addToCartItem.name === cartObject.name) {
         cartArray[i].quantValue = Math.floor(this.quantValue);
         break;
       }
     }
     localStorage.setItem('cart', JSON.stringify(cartArray));
+    this.cartArray = cartArray;
   }
   DeleteObject(objectToDelete) {
     const cartArray = JSON.parse(localStorage.getItem('cart')) || [];
@@ -37,13 +38,35 @@ export class CartComponent implements OnInit {
     this.cartArray = cartArray;
 
   }
+
+  CalcSubTotal() {
+    let calcvar = 0;
+    const cartArray = JSON.parse(localStorage.getItem('cart')) || [];
+// tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < cartArray.length; i++) {
+      calcvar += cartArray[i].addToCartItem.price * cartArray[i].quantValue;
+    }
+    return calcvar;
+  }
+  calcTaxes(){
+    let subtotal = this.CalcSubTotal();
+    subtotal = subtotal * 17 / 100;
+    return parseInt(subtotal.toFixed(2));
+  }
+  cartTotalFunc(){
+    let CartTotalSum: number;
+    CartTotalSum = this.CalcSubTotal() + this.calcTaxes() + this.ShippingValue;
+    return CartTotalSum.toFixed(2);
+  }
+  Checkout(){
+    alert('All done :)');
+  }
   constructor() { }
   calcLineItem(price: number, quantity: number) {
     return price * quantity;
   }
   ngOnInit() {
     this.cartArray = JSON.parse(localStorage.getItem('cart'));
-
   }
 
 }
