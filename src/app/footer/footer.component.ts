84f038a68b09737fe644b787e083e92c15db1213
avitalar <avitalar1@gmail.com>
2019-06-26 23:51:27 +0300

    foogter form

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Form} from '../form';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+model = new Form(1, 'name', 'email', 'message');
+
+submitted = false;
+
+onSubmit(event: Event) {
+  event.preventDefault();
+  this.submitted = true; }
   constructor() { }
 
   ngOnInit() {
